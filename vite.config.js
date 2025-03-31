eed1aba8d41b10a20fdca823a78248ad2d02ecac
LucasNovosado <165840126+LucasNovosado@users.noreply.github.com>
2025-03-31 13:46:22 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,22 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
   },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  },
+  define: {
+    '__APP_CONFIG__': JSON.stringify({
+      PARSE_APP_ID: "UKkWqowhXy7crlvQqlZdfcxJoWiMwEWVdZUSz5dN",
+      PARSE_JS_KEY: "errtZmuEsUserCUZrZIP2iYldNsw3jzqBzkrHSW9",
+      PARSE_SERVER_URL: "https://parseapi.back4app.com"
+    })
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
