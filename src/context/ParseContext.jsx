21b4cb821e5171dcecd8db3a7ebeb6fb8dbc1759
LucasNovosado <165840126+LucasNovosado@users.noreply.github.com>
2025-03-31 11:39:51 -0300

    Update ParseContext.jsx

--- src/context/ParseContext.jsx
+++ src/context/ParseContext.jsx
@@ -1,7 +1,7 @@
 // src/context/ParseContext.jsx
 
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import Parse from 'parse';
+import Parse, { initializeParse } from '../services/parseInitializer';
 
 // Criar contexto
 const ParseContext = createContext(null);
