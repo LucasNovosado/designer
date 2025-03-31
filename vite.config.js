3ca97e2c3abf5bcf25d6a2bf624840ad3f892ab0
LucasNovosado <165840126+LucasNovosado@users.noreply.github.com>
2025-03-31 13:47:19 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000,
-    host: '0.0.0.0'
-  },
-  preview: {
-    port: process.env.PORT || 3000,
-    host: '0.0.0.0'
-  },
-  define: {
-    '__APP_CONFIG__': JSON.stringify({
-      PARSE_APP_ID: "UKkWqowhXy7crlvQqlZdfcxJoWiMwEWVdZUSz5dN",
-      PARSE_JS_KEY: "errtZmuEsUserCUZrZIP2iYldNsw3jzqBzkrHSW9",
-      PARSE_SERVER_URL: "https://parseapi.back4app.com"
-    })
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false
+export default defineConfig(({ mode }) => {
+  // Carrega as variáveis de ambiente
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: env.PORT || 3000,
+      host: '0.0.0.0'
+    },
+    preview: {
+      port: env.PORT || 3000,
+      host: '0.0.0.0'
+    },
+    define: {
+      '__APP_CONFIG__': JSON.stringify({
+        PARSE_APP_ID: env.VITE_PARSE_APP_ID,
+        PARSE_JS_KEY: env.VITE_PARSE_JS_KEY,
+        PARSE_SERVER_URL: env.VITE_PARSE_SERVER_URL
+      })
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: false
+    }
   }
 })
