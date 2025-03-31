63e214443d30e954da41122ae68bf6071c5f001f
LucasNovosado <165840126+LucasNovosado@users.noreply.github.com>
2025-03-31 11:52:40 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
   },
-  build: {
-    minify: 'terser',
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        chunkSizeWarningLimit: 2000
-      }
-    },
-    terserOptions: {
-      compress: {
-        drop_console: false,
-        pure_funcs: ['console.debug']
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      'react-dom': 'react-dom'
-    }
-  }
 })
